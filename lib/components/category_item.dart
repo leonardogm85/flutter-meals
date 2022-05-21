523b8cd8896e853dbf78e8c155b35e34ae8d59f1
Leonardo Martins <leonardogm85@gmail.com>
2022-05-21 00:21:36 -0300

    Componente Item Categoria

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,6 +11,20 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(category.title);
+    return Container(
+      child: Text(category.title),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
   }
 }
